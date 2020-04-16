c690d63b07978da24c5f7da59fffbcf1decae92c
余航 <yh360757187@gmail.com>
2020-04-16 13:51:25 +0800

    添加打包文件

---
+++ react/my-app/build/precache-manifest.901cf9e74b1870fcb956e2f4a515e76e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "19a2ee8f65dc457a6f4c",
+    "url": "/static/js/main.19a2ee8f.chunk.js"
+  },
+  {
+    "revision": "abd9e857442f04b96f9b",
+    "url": "/static/js/1.abd9e857.chunk.js"
+  },
+  {
+    "revision": "19a2ee8f65dc457a6f4c",
+    "url": "/static/css/main.18b8eccc.chunk.css"
+  },
+  {
+    "revision": "439872097c0e0ecf8db79eae35b310c5",
+    "url": "/index.html"
+  }
+];
